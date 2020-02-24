0857e0872a852ce9fb2afef7e0ab6d9fceaba483
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-24 17:01:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,8 +62,5 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.students_below_12th_grade
-    sql = "SELECT * FROM students WHERE grade < 12"
-    DB[:conn].execute(sql)
-  end
+
 end
